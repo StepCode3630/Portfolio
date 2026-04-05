94cf0fb696b69bad182233a6583bffa80346dc4c
Stepan <pl77sbr@eduvaud.ch>
2026-04-05 18:17:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'portfolio',
+  base: '/Portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
