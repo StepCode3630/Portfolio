13fcc6385e225d77e91b588d8e91ed2dcf2931f9
Stepan <stepanpatricny@gmail.com>
2026-05-12 09:01:59 +0200

    feat: ajout d'info sur description et skills en const

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,3 +1,59 @@
-<script setup></script>
+<script setup>
+const Description =
+  'As a passionate web ( and others things:) developer, I create modern and dynamic user interfaces. I combine design and development to build simple, efficient, and enjoyable app experiences.'
+const skills = [
+  {
+    name: 'HTML',
+    type: 'Frontend',
+    icone: '/public/html5-original.svg',
+  },
+  {
+    name: 'CSS',
+    type: 'Frontend',
+
+    icone: '/public/css3-original.svg',
+  },
+  {
+    name: 'JavaScript',
+    type: 'Frontend',
+    icone: '/public/javascript-original.svg',
+  },
+  {
+    name: 'Vue.js',
+    type: 'Frontend',
+    icone: '/public/vuejs-original.svg',
+  },
+  {
+    name: 'node.js',
+    type: 'Backend',
+    icone: '/public/nodejs-original.svg',
+  },
+  {
+    name: 'Express.js',
+    type: 'Backend',
+    icone: '/public/express-original.svg',
+  },
+  {
+    name: 'Adonis.js',
+    type: 'Backend',
+    icone: '/public/adonisjs-original.svg',
+  },
+  {
+    name: 'C#',
+    type: 'Mobile',
+    icone: '/public/csharp-original.svg',
+  },
+  {
+    name: 'mongoDB',
+    type: 'Database',
+    icone: '/public/mongodb-original.svg',
+  },
+  {
+    name: 'MySQL',
+    type: 'Database',
+    icone: '/public/mysql-original.svg',
+  },
+]
+</script>
 <template></template>
 <style scoped></style>
